6813f44937f194280f9898a94015c93a5e26911b
aresder <anggariadisyahputra@gmail.com>
2025-03-20 12:09:33 +0700

    init

---
+++ be/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
